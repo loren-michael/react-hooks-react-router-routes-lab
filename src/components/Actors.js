b697de50ef5d5194776cd453921594b9137cada5
loren <lorenmichaelh@gmail.com>
2022-04-27 16:37:00 -0500

    completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actor) => {
+        return(
+          <div key={actor.name}>
+            {actor.name}
+            <ul>
+              {actor.movies.map((mov) => {
+                return <li key={mov}>{mov}</li>
+              })}
+            </ul>
+          </div>
+        )
+      })}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,24 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((mov) => {
+        return (
+          <div key={mov.title}>
+            <h4>{mov.title}</h4>
+            <p>{mov.time} minutes</p>
+            <ul>
+              {mov.genres.map((genre, index) => {
+                return <li key={index}>{genre}</li>
+              })}
+            </ul>
+          </div>
+        )
+      })}
+    </div>
+  );
 }
 
 export default Movies;
